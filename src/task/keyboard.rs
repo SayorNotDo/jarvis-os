4fd13757061df0f85792fdab339ad136e9a38fba
sayornott <jcskye07@hotmail.com>
2024-08-16 01:59:41 +0800

    keyboard related code modified

--- src/task/keyboard.rs
+++ src/task/keyboard.rs
@@ -1,17 +1,43 @@
+use core::pin::Pin;
+use core::task::{Context, Poll};
 use conquer_once::spin::OnceCell;
 use crossbeam::queue::ArrayQueue;
+use futures_util::{stream::Stream, StreamExt, task::AtomicWaker};
+use pc_keyboard::{DecodedKey, HandleControl, layouts, ScancodeSet1, Keyboard};
 
-use crate::println;
+use crate::{print, println};
 
 static SCANCODE_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
+static WAKER: AtomicWaker = AtomicWaker::new();
+
 
 pub(crate) fn add_scancode(scancode: u8) {
     if let Ok(queue) = SCANCODE_QUEUE.try_get() {
         if let Err(_) = queue.push(scancode) {
             println!("WARNING: scancode queue full; dropping keyboard input");
+        } else {
+            WAKER.wake();
         }
     } else {
-        println!("WARNING: scancode queue unintialized");
+        println!("WARNING: scancode queue uninitialized");
+    }
+}
+
+pub async fn print_keypresses() {
+    let mut scancodes = ScancodeStream::new();
+    let mut keyboard = Keyboard::new(ScancodeSet1::new(),
+                                     layouts::Us104Key, HandleControl::Ignore,
+    );
+
+    while let Some(scancode) = scancodes.next().await {
+        if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+            if let Some(key) = keyboard.process_keyevent(key_event) {
+                match key {
+                    DecodedKey::Unicode(character) => print!("{}", character),
+                    DecodedKey::RawKey(key) => print!("{}", key),
+                }
+            }
+        }
     }
 }
 
@@ -25,5 +51,26 @@ impl ScancodeStream {
             .try_init_once(|| ArrayQueue::new(100))
             .expect("ScancodeStream::new should only be called once");
         ScancodeStream { _private: () }
+    }
+}
+
+impl Stream for ScancodeStream {
+    type Item = u8;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<u8>> {
+        let queue = SCANCODE_QUEUE
+            .try_get()
+            .expect("scancode queue not initialized");
+        if let Some(scancode) = queue.pop() {
+            return Poll::Ready(Some(scancode));
+        }
+        WAKER.register(&cx.waker());
+        match queue.pop() {
+            Some(scancode) => {
+                WAKER.take();
+                Poll::Ready(Some(scancode))
+            }
+            None => Poll::Pending
+        }
     }
 }
